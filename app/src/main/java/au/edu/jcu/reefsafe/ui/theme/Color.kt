dbe4609e65c5b6210f67c29af11c58482953d5a0
emris05 <maythin.htet@my.jcu.edu.au>
2026-06-19 01:55:36 +0700

    gave the theme a reef colour palette

--- app/src/main/java/au/edu/jcu/reefsafe/ui/theme/Color.kt
+++ app/src/main/java/au/edu/jcu/reefsafe/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package au.edu.jcu.reefsafe.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val ReefTeal40 = Color(0xFF006A6A)
+val ReefTeal80 = Color(0xFF6FF6F6)
+val Coral40 = Color(0xFF8E4A40)
+val Coral80 = Color(0xFFFFB4A6)
+val Sand40 = Color(0xFF6E5C3E)
+val Sand80 = Color(0xFFFADF9F)
